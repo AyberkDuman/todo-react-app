1e541aebdad7120f9e756f8cea746c2dfdf511f5
Ayberk Duman <ayberk97.au@gmail.com>
2022-01-14 22:12:02 +0300

    i delete my firebaseConfig

--- todo-react-app/src/firebase/index.js
+++ todo-react-app/src/firebase/index.js
@@ -7,12 +7,7 @@ import { getFirestore } from '@firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBL7wZOeFuOVRXf10tM-fkPKP_EcN8useU",
-  authDomain: "todo-react-app-c91dc.firebaseapp.com",
-  projectId: "todo-react-app-c91dc",
-  storageBucket: "todo-react-app-c91dc.appspot.com",
-  messagingSenderId: "220385978245",
-  appId: "1:220385978245:web:9526aa1c0be4c9c0f846ec"
+  // add own firebaseConfig 
 };
 
 // Initialize Firebase
